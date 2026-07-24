2004636e19202110cbf10b665590919ba0cded6f
ahmed-x86 <ahmdansmr7@gmail.com>
2026-07-24 10:48:21 +0300

    .

---
+++ theme.h
@@ -0,0 +1,43 @@
+#ifndef THEME_H
+#define THEME_H
+
+#include <QString>
+#include <QDialog>
+#include <QVBoxLayout>
+
+enum class Language { Arabic, English };
+enum class ThemeMode { Mocha, Latte, ShamelaClassic, Dracula, Nord, Gruvbox };
+
+struct ThemeColors {
+    QString Base, Text, Surface, Hover;
+    QString Accent1, Accent2, Accent3, Accent4, Danger;
+    bool isDark; 
+};
+
+extern const ThemeColors MochaTheme;
+extern const ThemeColors LatteTheme;
+extern const ThemeColors ShamelaClassicTheme;
+extern const ThemeColors DraculaTheme;
+extern const ThemeColors NordTheme;
+extern const ThemeColors GruvboxTheme;
+
+
+class ThemeSelectionDialog : public QDialog {
+    Q_OBJECT
+public:
+    int selectedTheme = -1;
+    explicit ThemeSelectionDialog(Language lang, QWidget* parent = nullptr);
+};
+
+
+class CustomThemeDialog : public QDialog {
+    Q_OBJECT
+public:
+    ThemeColors m_colors;
+    explicit CustomThemeDialog(const ThemeColors& initialColors, QWidget* parent = nullptr);
+
+private:
+    void addColorRow(QVBoxLayout* layout, const QString& labelText, QString* colorRef);
+};
+
+#endif
